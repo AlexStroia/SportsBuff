fa7b5fb113ad2ccebb07f74b41a81591d7deb904
Alex S <alex.s@smarthuts.com>
2020-09-30 11:31:43 +0300

    SportsBuffError.kt improvments

--- app/src/main/java/com/buffup/app/core/SportsBuffError.kt
+++ app/src/main/java/com/buffup/app/core/SportsBuffError.kt
@@ -1,7 +1,17 @@
 package com.buffup.app.core
 
+import java.io.IOException
+
+class ApiException(errorMessage: String) : IOException(errorMessage)
+
 sealed class SportsBuffError {
     object NoInternet : SportsBuffError()
     object Unknown : SportsBuffError()
     data class Api(val message: String) : SportsBuffError()
+}
+
+fun Throwable.asError(): SportsBuffError = when (this) {
+    is ApiException ->SportsBuffError.Api(message.orEmpty())
+    is IOException -> SportsBuffError.NoInternet
+    else -> SportsBuffError.Unknown
 }
