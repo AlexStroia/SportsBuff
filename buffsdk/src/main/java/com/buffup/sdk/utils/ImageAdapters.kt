b9de4f6275b85a6cdd03e70710fd2721f0598265
Alex S <alex.s@smarthuts.com>
2020-10-02 21:11:22 +0300

    Fixes

--- buffsdk/src/main/java/com/buffup/sdk/utils/ImageAdapters.kt
+++ buffsdk/src/main/java/com/buffup/sdk/utils/ImageAdapters.kt
@@ -1,13 +1,33 @@
 package com.buffup.sdk.utils
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.buffup.sdk.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+
+@SuppressLint("CheckResult")
+private fun buildRequestOptionsForAnswer(): RequestOptions {
+    val requestOptions = RequestOptions()
+    requestOptions.error(R.drawable.ic_generic_answer)
+    return requestOptions
+}
 
 @BindingAdapter("setImage")
 fun ImageView.setImage(url: String?) {
     if (!url.isNullOrEmpty()) {
         Glide.with(this)
+            .load(url).into(this)
+    }
+}
+
+@BindingAdapter("setImageAnswer")
+fun ImageView.setImageAnswer(url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(this)
+            .setDefaultRequestOptions(buildRequestOptionsForAnswer())
             .load(url).into(this)
     }
 }
